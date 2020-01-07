e06a93f397bfa9e45b53ce2096ed94644ede396d
Ben Chartrand <ben@spottersystems.com>
2020-01-07 20:53:49 +1300

    built a confirmation dialog component

---
+++ components/ConfirmationDialog.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-dialog v-model="showDialog" :persistent="persistent" max-width="400">
+    <v-card>
+      <v-card-title class="headline">{{ title }}</v-card-title>
+      <v-card-text>{{ text }}</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="onConfirmationBtn()" text>{{
+          confirmationBtnText
+        }}</v-btn>
+        <v-btn @click="onCancelBtn()" text>{{ cancelBtnText }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'ConfirmationDialog',
+
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: null
+    },
+    text: {
+      type: String,
+      default: null
+    },
+    confirmationBtnText: {
+      type: String,
+      default: 'Yes'
+    },
+    cancelBtnText: {
+      type: String,
+      default: 'No'
+    },
+    persistent: {
+      type: Boolean,
+      default: false
+    },
+    id: {
+      // ID of the item you may be deleting, for example
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    onConfirmationBtn() {
+      this.$emit('on-dialog-confirmation', this.id)
+    },
+    onCancelBtn() {
+      this.$emit('on-dialog-cancel')
+    }
+  }
+}
+</script>
