a0754abea6062f0b25e1fc07312e2390379bce98
Ben Chartrand <ben@spottersystems.com>
2020-01-22 22:12:43 +1300

    got the function working

--- functions/index.js
+++ functions/index.js
@@ -39,88 +39,38 @@ exports.websiteScreenshots = functions
   .runWith(runtimeOpts)
   .firestore.document('organizations/{organizationId}/lessons/{lessonId}')
   .onWrite(async function(change, context) {
-    try {
-      const originalLesson = change.before.data()
-      const updatedLesson = change.after.data()
-      const bucket = admin.storage().bucket()
+    const originalLesson = change.before.data()
+    const updatedLesson = change.after.data()
+    const bucket = admin.storage().bucket()
 
-      if (!change.after.exists) {
-        // The document was deleted
-        console.debug('The document was deleted so delete the thumbnail')
-        await bucket.file(`screenshots/${originalLesson.id}`).delete()
-      } else if (!originalLesson.url && !updatedLesson.url) {
-        // It's still empty so do nothing
-        console.debug('No changes to URL so doing nothing')
-      } else if (originalLesson.url && !updatedLesson.url) {
-        // User deleted the URL - deleting the image
-        console.debug(`Deleting file ${originalLesson.url}`)
-        await bucket.file(`screenshots/${originalLesson.id}`).delete()
-        change.after.ref.set({ url_thumbnail: null }, { merge: true })
-      } else if (!originalLesson.url && updatedLesson.url) {
-        // User added a URL to an existing lesson, when previously there was none, so let's process it
-        console.debug(`User added a different URL ${updatedLesson.url}`)
-        await saveScreenshot(context.params.lessonId, updatedLesson.url)
-      } else if (originalLesson.url !== updatedLesson.url) {
-        // URL changed so delete the previous image and create a new one
-        console.debug(
-          `Deleting file ${originalLesson.url} and creating a new thumbnail`
-        )
-        await bucket.file(`screenshots/${originalLesson.id}`).delete()
+    if (change.before.exists && !change.after.exists) {
+      // The whole document was deleted
+      console.log('The document was deleted so delete the thumbnail')
+      await bucket.file(`screenshots/${originalLesson.id}`).delete()
+    } else if (!originalLesson.url && !updatedLesson.url) {
+      // It's still empty so do nothing
+      console.log('No changes to URL so doing nothing')
+    } else if (originalLesson.url && !updatedLesson.url) {
+      // User deleted the URL - deleting the image
+      console.log(`Deleting file ${originalLesson.url}`)
+      await bucket.file(`screenshots/${context.params.lessonId}.png`).delete()
+      change.after.ref.set({ url_thumbnail: null }, { merge: true })
+    } else if (!originalLesson.url && updatedLesson.url) {
+      // User added a URL to an existing lesson, when previously there was none, so let's process it
+      console.log(
+        `User added a URL when one didn't exist previously ${updatedLesson.url}`
+      )
+      await saveScreenshot(context.params.lessonId, updatedLesson.url)
+    } else if (originalLesson.url !== updatedLesson.url) {
+      // URL changed so delete the previous image and create a new one
+      console.log(
+        `Deleting file ${originalLesson.url} and creating a new thumbnail`
+      )
+      await bucket.file(`screenshots/${context.params.lessonId}.png`).delete()
 
-        await saveScreenshot(context.params.lessonId, updatedLesson.url)
-      }
-    } catch (error) {
-      console.error(error)
-    }
-  })
-
-exports.takeScreenshotOnCreate = functions
-  .runWith(runtimeOpts)
-  .firestore.document('organizations/{organizationId}/lessons/{lessonId}')
-  .onCreate(async function(event, context) {
-    try {
-      const lesson = event.data()
-      if (lesson.url) {
-        await saveScreenshot(lesson)
-      }
-    } catch (error) {
-      console.error(error)
-    }
-  })
-
-exports.takeScreenshotOnUpdate = functions
-  .runWith(runtimeOpts)
-  .firestore.document('organizations/{organizationId}/lessons/{lessonId}')
-  .onUpdate(async function(change, context) {
-    try {
-      const originalLesson = change.before.data()
-      const updatedLesson = change.after.data()
-      const bucket = admin.storage().bucket()
-
-      if (!originalLesson.url && !updatedLesson.url) {
-        // It's still empty so do nothing
-        console.debug('No changes to URL so doing nothing')
-      } else if (originalLesson.url && !updatedLesson.url) {
-        console.debug(`User deleted the URL - deleting the image`)
-        console.debug(`Deleting file ${originalLesson.url}`)
-        await bucket.file(`screenshots/${originalLesson.id}`).delete()
-        change.after.ref.set({ url_thumbnail: null }, { merge: true })
-      } else if (!originalLesson.url && updatedLesson.url) {
-        console.debug(
-          `User added a URL to an existing lesson, when previously there was none, so let's process it`
-        )
-        await saveScreenshot(context.params.lessonId, updatedLesson.url)
-      } else if (originalLesson.url !== updatedLesson.url) {
-        console.debug(
-          `URL changed so delete the previous image and create a new one`
-        )
-        console.debug(`Deleting file ${originalLesson.url}`)
-        await bucket.file(`screenshots/${originalLesson.id}`).delete()
-
-        await saveScreenshot(context.params.lessonId, updatedLesson.url)
-      }
-    } catch (error) {
-      console.error(error)
+      await saveScreenshot(context.params.lessonId, updatedLesson.url)
+    } else {
+      console.log('Nothing happened')
     }
   })
 
@@ -136,7 +86,7 @@ async function saveScreenshot(id, url) {
   await page.setViewport({ width: 1024, height: 576 })
 
   // Go to your website
-  console.debug(`Going to ${url}`)
+  console.log(`Going to ${url}`)
   await page.goto(url)
 
   // Disable service workers
@@ -152,11 +102,11 @@ async function saveScreenshot(id, url) {
 
   // Create a file object
   const filePath = `screenshots/${id}.png`
-  console.debug(`Creating file at ${filePath}`)
+  console.log(`Creating file at ${filePath}`)
   const file = bucket.file(filePath)
 
   // Save the image
-  console.debug(`Saving file`)
+  console.log(`Saving file`)
   await file.save(screenshot)
 
   return filePath
