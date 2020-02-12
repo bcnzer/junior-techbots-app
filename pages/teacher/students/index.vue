ca4805386b4881578b83ad099900ee354dd1493c
Ben Chartrand <ben@spottersystems.com>
2020-02-12 22:19:19 +1300

    fixed inviting of students

--- pages/teacher/students/index.vue
+++ pages/teacher/students/index.vue
@@ -8,113 +8,120 @@
 
     <v-row v-if="!loading">
       <v-col cols="12" xs="12" class="mx-auto">
-        <v-data-table :headers="headers" :items="students">
-          <template v-slot:top>
-            <v-toolbar flat color="white">
-              <v-toolbar-title>Students</v-toolbar-title>
-              <v-spacer></v-spacer>
+        <v-card>
+          <v-card-title>
+            Students
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+              class="mr-2 mb-2"
+            ></v-text-field>
 
-              <student-entry-form
-                :entry-form-id="entryFormId"
-                :entry-form-enabled="entryFormEnabled"
-                :entry-form-message="entryFormMessage"
-                v-on:save-event-form="onSaveEntryFormDetails"
-              ></student-entry-form>
-              <v-dialog v-model="showEmailDialog" max-width="500px">
-                <template v-slot:activator="{ on }">
-                  <v-btn v-on="on">Invite Student</v-btn>
-                </template>
-                <v-form ref="modalInviteStudent" lazy-validation>
-                  <v-card>
-                    <v-card-title>
-                      <span class="headline">Invite Student</span>
-                    </v-card-title>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col cols="12">
-                            <v-text-field
-                              v-model="dialogEmail"
-                              :rules="dialogEmailRules"
-                              :disabled="saving"
-                              label="Email"
-                              required
-                            ></v-text-field>
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn
-                        @click="inviteStudent()"
-                        :loading="saving"
-                        color="primary"
-                        >Save</v-btn
-                      >
-                      <v-btn @click="showEmailDialog = false" :disabled="saving"
-                        >Cancel</v-btn
-                      >
-                    </v-card-actions>
-                  </v-card>
-                </v-form>
-              </v-dialog>
+            <student-entry-form
+              :entry-form-id="entryFormId"
+              :entry-form-enabled="entryFormEnabled"
+              :entry-form-message="entryFormMessage"
+              v-on:save-event-form="onSaveEntryFormDetails"
+            ></student-entry-form>
+            <v-dialog v-model="showEmailDialog" max-width="500px">
+              <template v-slot:activator="{ on }">
+                <v-btn v-on="on">Invite Student</v-btn>
+              </template>
+              <v-form ref="modalInviteStudent" lazy-validation>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">Invite Student</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="dialogEmail"
+                            :rules="dialogEmailRules"
+                            :disabled="saving"
+                            label="Email"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      @click="inviteStudent()"
+                      :loading="saving"
+                      color="primary"
+                      >Save</v-btn
+                    >
+                    <v-btn @click="showEmailDialog = false" :disabled="saving"
+                      >Cancel</v-btn
+                    >
+                  </v-card-actions>
+                </v-card>
+              </v-form>
+            </v-dialog>
 
-              <v-dialog
-                v-model="showStudentEditDialog"
-                persistent
-                max-width="500px"
-              >
-                <v-form ref="modalEditStudent" lazy-validation>
-                  <v-card>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col cols="12">
-                            <v-text-field
-                              v-model="dialogStudentDisplayName"
-                              :disabled="saving"
-                              :rules="[(v) => !!v || 'Name is required']"
-                              label="Name"
-                              required
-                            ></v-text-field>
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn
-                        @click="editStudent()"
-                        :loading="saving"
-                        color="primary"
-                        >Save</v-btn
-                      >
-                      <v-btn
-                        @click="showStudentEditDialog = false"
-                        :disabled="saving"
-                        >Cancel</v-btn
-                      >
-                    </v-card-actions>
-                  </v-card>
-                </v-form>
-              </v-dialog>
-            </v-toolbar>
-          </template>
+            <v-dialog
+              v-model="showStudentEditDialog"
+              persistent
+              max-width="500px"
+            >
+              <v-form ref="modalEditStudent" lazy-validation>
+                <v-card>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="dialogStudentDisplayName"
+                            :disabled="saving"
+                            :rules="[(v) => !!v || 'Name is required']"
+                            label="Name"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      @click="editStudent()"
+                      :loading="saving"
+                      color="primary"
+                      >Save</v-btn
+                    >
+                    <v-btn
+                      @click="showStudentEditDialog = false"
+                      :disabled="saving"
+                      >Cancel</v-btn
+                    >
+                  </v-card-actions>
+                </v-card>
+              </v-form>
+            </v-dialog>
+          </v-card-title>
+          <v-data-table :headers="headers" :items="students">
+            <template slot="no-data">
+              No students have joined. Invite some!
+            </template>
 
-          <template slot="no-data">
-            No students have joined. Invite some!
-          </template>
-
-          <template v-slot:item.action="{ item }">
-            <v-icon @click="showEditStudent(item)" small class="mr-3">
-              mdi-pencil
-            </v-icon>
-            <v-icon @click="removeStudent(item)" small>
-              mdi-delete
-            </v-icon>
-          </template>
-        </v-data-table>
+            <template v-slot:item.action="{ item }">
+              <v-icon @click="showEditStudent(item)" small class="mr-3">
+                mdi-pencil
+              </v-icon>
+              <v-icon @click="removeStudent(item)" small>
+                mdi-delete
+              </v-icon>
+            </template>
+          </v-data-table>
+        </v-card>
       </v-col>
       <snackbar />
     </v-row>
@@ -123,6 +130,7 @@
 
 <script>
 import firebase from 'firebase/app'
+import uuidv4 from 'uuid/v4'
 import { firestore } from '@/services/fireinit.js'
 import snackbar from '@/components/snackbar'
 import StudentEntryForm from '@/components/teacher/StudentEntryForm'
@@ -154,6 +162,7 @@ export default {
       showSnackbar: false,
       headers: [
         { text: 'Name', value: 'name' },
+        { text: 'Email', value: 'email' },
         { text: 'Class', value: 'class' },
         { text: 'Actions', value: 'action', sortable: false }
       ],
@@ -182,6 +191,7 @@ export default {
             uid: student.uid,
             recordId: doc.id,
             name: student.displayName,
+            email: student.email,
             class: ''
           })
         })
@@ -261,10 +271,10 @@ export default {
       if (studentRecord.docs.length === 0) {
         // Add the student record and the invite
         const newStudent = await firestore.collection('students').add({
-          email: this.dialogEmail,
-          organizations: firebase.firestore.FieldValue.arrayUnion(
-            JSON.parse(localStorage.org).id
-          )
+          email: this.dialogEmail
+          // organizations: firebase.firestore.FieldValue.arrayUnion(
+          //   JSON.parse(localStorage.org).id
+          // )
         })
 
         studentId = newStudent.id
@@ -278,6 +288,7 @@ export default {
         .doc(studentId)
         .collection('invites')
         .add({
+          inviteId: uuidv4().substring(0, 8),
           invitedOn: new Date(),
           invitedBy: JSON.parse(localStorage.currentUser).uid,
           email: this.dialogEmail,
