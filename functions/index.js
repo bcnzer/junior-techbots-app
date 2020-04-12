9882c085c00faaa5835415cf5f19cbcca005647a
Ben Chartrand <ben@spottersystems.com>
2020-04-12 13:55:05 +1200

    Added URL to docs

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const inviteStudent = require('./inviteStudent')
 const screenshots = require('./screenshots')
 
+// More info on organizing functions https://firebase.google.com/docs/functions/organize-functions
 exports.inviteStudentEmail = inviteStudent.inviteStudentEmail
 exports.websiteScreenshots = screenshots.websiteScreenshots
