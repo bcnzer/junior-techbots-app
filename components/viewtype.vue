b560db9a3b7edff29e2515f7ef913ffe6d6ed9ef
Ben Chartrand <ben@spottersystems.com>
2020-04-13 10:14:53 +1200

    improved viewtype

--- components/viewtype.vue
+++ components/viewtype.vue
@@ -1,7 +1,13 @@
 <template>
   <v-hover v-slot:default="{ hover }">
     <v-card :elevation="hover ? 16 : 4" class="mx-auto py-2" max-width="300">
-      <v-img :src="image" max-width="200" class="mx-auto"></v-img>
+      <v-img
+        :src="image"
+        aspect-ratio="1"
+        max-width="200"
+        max-height="200"
+        class="mx-auto"
+      ></v-img>
 
       <v-card-actions>
         <v-btn
