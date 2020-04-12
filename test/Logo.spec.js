c935ccae1085d067da01cf59f69d4ba7df9b3097
Ben Chartrand <ben@spottersystems.com>
2020-04-13 09:12:57 +1200

    removed test spec files

--- test/Logo.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import snackbar from '@/components/snackbar.vue'
-
-describe('Logo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(snackbar)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
