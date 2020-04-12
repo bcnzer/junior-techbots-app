cc17fafba2789771933bc82e60aea97c71a8bdb5
Ben Chartrand <ben@spottersystems.com>
2020-04-12 13:50:49 +1200

    Broke functions into separate files

---
+++ functions/inviteStudent.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions')
+const sendGridEmail = require('@sendgrid/mail')
+
+const SENDGRID_API_KEY = functions.config().sendgrid.key
+sendGridEmail.setApiKey(SENDGRID_API_KEY)
+
+exports.inviteStudentEmail = functions.firestore
+  .document('students/{studentId}/invites/{inviteId}')
+  .onCreate((event, context) => {
+    const invite = event.data()
+    const msg = {
+      to: invite.email,
+      from: 'Junior Techbots <noreply@juniortechbots.com>',
+      subject: `You've been invited to ${invite.orgName}`,
+      templateId: 'd-ce3cf6acb54549999cb13c7db296140c',
+      dynamic_template_data: {
+        orgName: invite.orgName,
+        orgId: invite.orgId,
+        studentId: invite.inviteId
+      }
+    }
+
+    return sendGridEmail
+      .send(msg)
+      .then(() => console.log(invite.orgName))
+      .catch((error) => console.error(error.toString()))
+  })
