fd2cf3914ace5881826813ceeacc417573b93d86
Ben Chartrand <ben@spottersystems.com>
2021-01-27 19:54:58 +1300

    placeholder page

---
+++ pages/student/_id/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12>
+      <div class="text-center my-5">
+        <v-img src="bot-hi.png" width="256"></v-img>
+      </div>
+    </v-flex>
+    <v-flex xs12>
+      <div class="headline">
+        Hello!
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  layout: 'minimal',
+
+  head() {
+    return { title: 'Student' }
+  }
+}
+</script>
