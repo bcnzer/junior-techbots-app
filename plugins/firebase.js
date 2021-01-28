40eb75720061997cfb8e70cffdc37581883b6f30
Ben Chartrand <ben@spottersystems.com>
2021-01-28 20:45:27 +1300

    force update of token if claims change

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,8 +1,16 @@
-import { auth } from '~/services/fireinit.js'
+import { firestore, auth } from '~/services/fireinit.js'
 
 export default (context) => {
   auth.onAuthStateChanged((user) => {
-    // user.getIdTokenResult().then(console.log)
     context.store.commit('user/setUser', user)
+
+    // Force the JWT to update if there is a change in claims information, which will then be picked up by rules
+    firestore
+      .collection('user_claims')
+      .doc(user.uid)
+      .onSnapshot(async () => {
+        // force a refresh of the ID token, which will pick up new claims
+        await user.getIdTokenResult(true)
+      })
   })
 }
